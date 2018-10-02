4af5ae4a9de07733466ebe375c67dae188211615
Jonathan Schoonhoven <jtschoonhoven@gmail.com>
2018-10-02 09:40:25 -0700

    resize

--- src/index.js
+++ src/index.js
@@ -11,8 +11,20 @@ const seriously = new Seriously();
 const source = seriously.source('camera');
 const target = seriously.target('canvas');
 const edgeEffect = seriously.effect('edge');
+const reformatTransform = seriously.transform('reformat');
 
-edgeEffect.source = source;
+function reformat() {
+    const devicePixelRatio = window.devicePixelRatio || 1;
+    target.width = window.innerWidth * devicePixelRatio;
+    target.height = window.innerHeight * devicePixelRatio;
+    reformatTransform.width = target.width;
+    reformatTransform.height = target.height;
+}
+window.onresize = reformat;
+reformat();
+
+reformatTransform.source = source;
+edgeEffect.source = reformatTransform;
 target.source = edgeEffect;
 
 seriously.go();
